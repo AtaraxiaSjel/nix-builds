15e9bcb6f754e1379ec3e9346e24dffc43e77a4e
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-12-01 23:04:34 +0300

    ci: add builds for elephant and walker

--- flake.nix
+++ flake.nix
@@ -1,16 +1,48 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+    systems.url = "github:nix-systems/default-linux";
     flake-utils.url = "github:numtide/flake-utils?ref=main";
     tuwunel = {
       url = "github:matrix-construct/tuwunel?ref=main";
       inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.attic.follows = "";
+      inputs.cachix.follows = "";
+    };
+    elephant = {
+      url = "github:abenz1267/elephant";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+    walker = {
+      url = "github:abenz1267/walker";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.elephant.follows = "elephant";
+      inputs.systems.follows = "systems";
     };
   };
   outputs =
     inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system: {
-      packages.default = inputs.tuwunel.packages.${system}.default.override {
-        features = [ "ldap" ];
-      };
-    });
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          tuwunel = inputs.tuwunel.packages.${system}.default.override {
+            features = [ "ldap" ];
+          };
+          elephant = inputs.elephant.packages.${system}.default;
+          walker = inputs.walker.packages.${system}.default;
+        };
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            jq
+            nix-fast-build
+          ];
+        };
+      }
+    );
 }
